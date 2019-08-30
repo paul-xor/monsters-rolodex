828e655b0f909d52875301d20917f4bc8a0c0f64
Pasha Smolov <palexsmol@gmail.com>
2019-08-29 20:05:27 -0700

    Updates

---
+++ precache-manifest.2b5a19f2eecac466de4f44514e5123b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651ce93588cd5ca0c247223f8cc7de55",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "659677c8d6255ed7b94f",
+    "url": "/monsters-rolodex/static/css/main.e5582ce3.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "659677c8d6255ed7b94f",
+    "url": "/monsters-rolodex/static/js/main.24a391c4.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
